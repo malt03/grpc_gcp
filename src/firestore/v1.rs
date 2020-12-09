bb280abd546ff6b770d797b0c796853107cd9cec
Koji Murata <malt.koji@gmail.com>
2020-12-09 11:29:23 +0900

    works

--- src/firestore/v1.rs
+++ src/firestore/v1.rs
@@ -1,3 +1,6 @@
+use once_cell::sync::OnceCell;
+use std::sync::{Arc, Mutex};
+
 use crate::proto::google::firestore::v1::{firestore_client::FirestoreClient, GetDocumentRequest};
 
 const URL: &str = "https://firestore.googleapis.com";
@@ -5,59 +8,48 @@ const DOMAIN: &str = "firestore.googleapis.com";
 
 type Client = FirestoreClient<tonic::transport::Channel>;
 
-#[derive(Debug)]
-struct ConnectionPool {
-    clients: Vec<Client>,
+static CLIENT: OnceCell<Arc<Mutex<Client>>> = OnceCell::new();
+
+async fn get_token() -> Result<gcp_auth::Token, Box<dyn std::error::Error>> {
+    let authentication_manager = gcp_auth::init().await?;
+    let token = authentication_manager
+        .get_token(&["https://www.googleapis.com/auth/datastore"])
+        .await?;
+    Ok(token)
 }
 
-impl ConnectionPool {
-    crate::singleton!(ConnectionPool);
-    fn new() -> ConnectionPool {
-        ConnectionPool {
-            clients: Vec::new(),
+async fn create_client() -> Result<Client, Box<dyn std::error::Error>> {
+    let tls = tonic::transport::ClientTlsConfig::new().domain_name(DOMAIN);
+    let channel = tonic::transport::Channel::from_static(URL)
+        .tls_config(tls)?
+        .connect()
+        .await?;
+    let token = get_token().await?;
+    let bearer = format!("Bearer {}", token.as_str());
+    let header_value = tonic::metadata::MetadataValue::from_str(&bearer)?;
+    let c = FirestoreClient::with_interceptor(channel, move |mut req: tonic::Request<()>| {
+        req.metadata_mut()
+            .insert("authorization", header_value.clone());
+        Ok(req)
+    });
+    Ok(c)
+}
+
+async fn get_client() -> Result<&'static Arc<Mutex<Client>>, Box<dyn std::error::Error>> {
+    let c = match CLIENT.get() {
+        Some(client) => client,
+        None => {
+            let client = create_client().await?;
+            CLIENT.set(Arc::new(Mutex::new(client))).unwrap();
+            CLIENT.get().unwrap()
         }
-    }
-
-    async fn get_token(&mut self) -> Result<gcp_auth::Token, Box<dyn std::error::Error>> {
-        let authentication_manager = gcp_auth::init().await?;
-        let token = authentication_manager
-            .get_token(&["https://www.googleapis.com/auth/datastore"])
-            .await?;
-        Ok(token)
-    }
-
-    async fn borrow_client(&mut self) -> Result<Client, Box<dyn std::error::Error>> {
-        if let Some(client) = self.clients.pop() {
-            println!("reuse client");
-            return Ok(client);
-        }
-        println!("create client");
-        let token = self.get_token().await?;
-        let bearer = format!("Bearer {}", token.as_str());
-        let header_value = tonic::metadata::MetadataValue::from_str(&bearer)?;
-        let tls = tonic::transport::ClientTlsConfig::new().domain_name(DOMAIN);
-        let channel = tonic::transport::Channel::from_static(URL)
-            .tls_config(tls)?
-            .connect()
-            .await?;
-        let client =
-            FirestoreClient::with_interceptor(channel, move |mut req: tonic::Request<()>| {
-                req.metadata_mut()
-                    .insert("authorization", header_value.clone());
-                Ok(req)
-            });
-        Ok(client)
-    }
-
-    fn return_client(&mut self, client: Client) {
-        self.clients.push(client);
-    }
+    };
+    Ok(c)
 }
 
 pub async fn get_document(path: impl Into<String>) -> Result<(), Box<dyn std::error::Error>> {
-    let pool = ConnectionPool::shared();
-
-    let mut client = pool.lock().unwrap().borrow_client().await?;
+    let c = get_client().await?;
+    let mut client = c.lock().unwrap();
 
     let request = tonic::Request::new(GetDocumentRequest {
         name: format!(
@@ -71,6 +63,5 @@ pub async fn get_document(path: impl Into<String>) -> Result<(), Box<dyn std::er
     let response = client.get_document(request).await?;
     println!("{:?}", response.get_ref().fields);
 
-    pool.lock().unwrap().return_client(client);
     Ok(())
 }
