21a9e6f6e821bc8d112b833bd30756f96e807783
Koji Murata <malt.koji@gmail.com>
2020-12-10 23:27:47 +0900

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use grpc_gcp;
+use std::io::{self, BufRead};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -7,22 +8,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let paths = vec![
         "/AllChatRoomUnreads/ExBBU90TPTR5DBg1Og0UFu4eCeZ2",
         "/Areas/0055a16bd49468b6df2cb8ea384c23ff",
-        "/ChatRooms/9VDq7RjLKhAiuRDbSeeT",
-        "/Pins/04NkBxn906NOb7LpSzh9",
-        "/Users/3CRKoFAOygcl7xK60WamxvcrjYY2",
     ];
 
-    let mut all = Vec::new();
-    for path in paths {
-        let future = grpc_gcp::firestore::v1::get_document(path);
-        all.push(future);
-    }
+    loop {
+        let stdin = io::stdin();
+        for _ in stdin.lock().lines() {
+            let mut all = Vec::new();
+            for path in paths.clone() {
+                let future = grpc_gcp::firestore::v1::get_document(path);
+                all.push(future);
+            }
 
-    let results = futures::future::join_all(all).await;
-    for result in results {
-        let response = result.unwrap();
-        println!("{:?}", response.get_ref().fields);
+            let results = futures::future::join_all(all).await;
+            for result in results {
+                let response = result.unwrap();
+                println!("{:?}", response.get_ref().fields);
+            }
+        }
     }
-
-    Ok(())
 }
