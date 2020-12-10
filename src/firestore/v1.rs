55752f30c44ed6eea2bb6b257e69edb084bcbea4
Koji Murata <malt.koji@gmail.com>
2020-12-10 12:37:50 +0900

    fix

--- src/firestore/v1.rs
+++ src/firestore/v1.rs
@@ -1,59 +1,83 @@
 use once_cell::sync::OnceCell;
-use std::sync::{Arc, Mutex};
 
-use crate::proto::google::firestore::v1::{firestore_client::FirestoreClient, GetDocumentRequest};
+use crate::proto::google::firestore::v1::{
+    firestore_client::FirestoreClient, Document, GetDocumentRequest,
+};
 
 const URL: &str = "https://firestore.googleapis.com";
 const DOMAIN: &str = "firestore.googleapis.com";
 
 type Client = FirestoreClient<tonic::transport::Channel>;
 
-static CLIENT: OnceCell<Arc<Mutex<Client>>> = OnceCell::new();
+static CHANNEL: OnceCell<tonic::transport::Channel> = OnceCell::new();
+static AUTHENTICATION_MANAGER: OnceCell<gcp_auth::AuthenticationManager> = OnceCell::new();
 
 async fn get_token() -> Result<gcp_auth::Token, Box<dyn std::error::Error>> {
-    let authentication_manager = gcp_auth::init().await?;
+    let authentication_manager = get_authentication_manager().await?;
     let token = authentication_manager
         .get_token(&["https://www.googleapis.com/auth/datastore"])
         .await?;
     Ok(token)
 }
 
-async fn create_client() -> Result<Client, Box<dyn std::error::Error>> {
+async fn create_channel() -> Result<tonic::transport::Channel, Box<dyn std::error::Error>> {
     let tls = tonic::transport::ClientTlsConfig::new().domain_name(DOMAIN);
     let channel = tonic::transport::Channel::from_static(URL)
         .tls_config(tls)?
         .connect()
         .await?;
+    Ok(channel)
+}
+
+async fn get_authentication_manager(
+) -> Result<&'static gcp_auth::AuthenticationManager, Box<dyn std::error::Error>> {
+    let manager = match AUTHENTICATION_MANAGER.get() {
+        Some(manager) => manager,
+        None => {
+            let manager = gcp_auth::init().await?;
+            if AUTHENTICATION_MANAGER.set(manager).is_err() {
+                panic!("unexpected");
+            }
+            AUTHENTICATION_MANAGER.get().unwrap()
+        }
+    };
+    Ok(manager)
+}
+
+async fn get_channel() -> Result<tonic::transport::Channel, Box<dyn std::error::Error>> {
+    let channel = match CHANNEL.get() {
+        Some(channel) => channel,
+        None => {
+            let channel = create_channel().await?;
+            CHANNEL.set(channel).unwrap();
+            CHANNEL.get().unwrap()
+        }
+    };
+    Ok(channel.clone())
+}
+
+async fn get_client() -> Result<Client, Box<dyn std::error::Error>> {
+    let channel = get_channel().await?;
     let token = get_token().await?;
+
     let bearer = format!("Bearer {}", token.as_str());
     let header_value = tonic::metadata::MetadataValue::from_str(&bearer)?;
-    let c = FirestoreClient::with_interceptor(channel, move |mut req: tonic::Request<()>| {
+    let client = FirestoreClient::with_interceptor(channel, move |mut req: tonic::Request<()>| {
         req.metadata_mut()
             .insert("authorization", header_value.clone());
         Ok(req)
     });
-    Ok(c)
+    Ok(client)
 }
 
-async fn get_client() -> Result<&'static Arc<Mutex<Client>>, Box<dyn std::error::Error>> {
-    let c = match CLIENT.get() {
-        Some(client) => client,
-        None => {
-            let client = create_client().await?;
-            CLIENT.set(Arc::new(Mutex::new(client))).unwrap();
-            CLIENT.get().unwrap()
-        }
-    };
-    Ok(c)
-}
-
-pub async fn get_document(path: impl Into<String>) -> Result<(), Box<dyn std::error::Error>> {
-    let c = get_client().await?;
-    let mut client = c.lock().unwrap();
+pub async fn get_document(
+    path: impl Into<String>,
+) -> Result<tonic::Response<Document>, Box<dyn std::error::Error>> {
+    let mut client = get_client().await?;
 
     let request = tonic::Request::new(GetDocumentRequest {
         name: format!(
-            "projects/{}/databases/(default)/documents/{}",
+            "projects/{}/databases/(default)/documents{}",
             crate::Config::shared().lock().unwrap().get_project_id(),
             path.into()
         )
@@ -61,7 +85,5 @@ pub async fn get_document(path: impl Into<String>) -> Result<(), Box<dyn std::er
         ..Default::default()
     });
     let response = client.get_document(request).await?;
-    println!("{:?}", response.get_ref().fields);
-
-    Ok(())
+    Ok(response)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,53 @@
 use grpc_gcp;
-use std::io::{self, BufRead};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     grpc_gcp::initialize("babyfood");
 
-    loop {
-        let stdin = io::stdin();
-        for line in stdin.lock().lines() {
-            let path = line.unwrap();
-            grpc_gcp::firestore::v1::get_document(path).await?;
-        }
+    let paths = vec![
+        "/AllChatRoomUnreads/ExBBU90TPTR5DBg1Og0UFu4eCeZ2",
+        "/Areas/0055a16bd49468b6df2cb8ea384c23ff",
+        "/ChatRooms/9VDq7RjLKhAiuRDbSeeT",
+        "/Pins/04NkBxn906NOb7LpSzh9",
+        "/Users/3CRKoFAOygcl7xK60WamxvcrjYY2",
+    ];
+
+    let mut all = Vec::new();
+    for path in paths {
+        let future = grpc_gcp::firestore::v1::get_document(path);
+        all.push(future);
     }
+
+    let results = futures::future::join_all(all).await;
+    for result in results {
+        let response = result.unwrap();
+        println!("{:?}", response.get_ref().fields);
+    }
+
+    Ok(())
 }
+
+// fn main() {
+//     let vec = vec![1, 2, 3, 4, 5];
+
+//     let mut rng = rand::thread_rng();
+//     let mut handles = Vec::new();
+
+//     for value in vec {
+//         let wait: u64 = rng.gen_range(100, 200);
+//         let handle = std::thread::spawn(move || {
+//             std::thread::sleep(std::time::Duration::from_millis(wait));
+//             println!("{}", value);
+//             value * 2
+//         });
+//         handles.push(handle);
+//     }
+
+//     std::thread::sleep(std::time::Duration::from_secs(5));
+
+//     let results: Vec<_> = handles
+//         .into_iter()
+//         .map(|handle| handle.join().unwrap())
+//         .collect();
+//     println!("{:?}", results);
+// }
