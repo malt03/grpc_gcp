c9e5f3a4569d732eefc3ba32eea2b19f17ab4853
Koji Murata <malt.koji@gmail.com>
2020-12-17 11:52:55 +0900

    works convert string and integer

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
-use grpc_gcp;
+use std::convert::TryFrom;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    grpc_gcp::init("malt03");
+use grpc_gcp::firestore::v1::serde_document;
+use serde::Deserialize;
 
-    let paths = vec!["/test/JM6W5nAExLohWQir079S", "/test/njavllVh8IctGwjyit2n"];
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     grpc_gcp::init("malt03");
 
-    let mut all = Vec::new();
-    for path in paths.clone() {
-        let future = grpc_gcp::firestore::v1::get_document(path);
-        all.push(future);
-    }
+//     let paths = vec!["/test/JM6W5nAExLohWQir079S", "/test/njavllVh8IctGwjyit2n"];
 
-    let results = futures::future::join_all(all).await;
-    for result in results {
-        let response = result.unwrap();
-        println!("{:?}", response.get_ref().fields);
-    }
+//     let mut all = Vec::new();
+//     for path in paths.clone() {
+//         let future = grpc_gcp::firestore::v1::get_document(path);
+//         all.push(future);
+//     }
 
-    Ok(())
+//     let results = futures::future::join_all(all).await;
+//     for result in results {
+//         let response = result.unwrap();
+//         println!("{:?}", response.get_ref().fields);
+//     }
+
+//     Ok(())
+// }
+
+#[derive(Deserialize, Debug)]
+struct Person {
+    age: String,
+    name: String,
+}
+
+fn main() {
+    let i: i64 = 2147483647;
+    let u: u8 = i as u8;
+    // let u: u32 = TryFrom::try_from(i);
+    println!("{}, {}", i, u);
+    // let data = r#"{"name":"John Doe","age":"29"}"#;
+
+    // let result: serde_document::Result<Person> = serde_document::from_fields(data);
+    // match result {
+    //     Err(e) => println!("{:?}", e),
+    //     Ok(p) => println!("{:?}", p),
+    // }
 }
